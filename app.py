b38a1b0be741ef9cf23a3911c89142a9fd9902a0
nottahagilani <43296138+nottahagilani@users.noreply.github.com>
2019-07-03 11:24:04 +0500

    1.21

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app = flask.Flask(__name__)
 # global instantiations
 global_graph = tensorflow.get_default_graph()
 places_cats = {}
+birds_cats = {}
 
 # loading labels
 fd = open('metadata/places_categories.txt', 'r')
@@ -27,6 +28,14 @@ for x in fd.readlines():
     places_cats[int(indx)] = cat
 fd.close()
 
+fd = open( 'metadata/birds_categories.txt' , 'r' )
+for x in fd.readlines():
+    y = x.split( ' ' )
+    cat = y[0]
+    indx = y[1].strip()
+    birds_cats[int( indx )] = cat
+fd.close()
+
 # loading model
 base_model = InceptionV3(weights=None, include_top=False)
 x = base_model.output
@@ -35,6 +44,14 @@ x = Dense(1024, activation='relu')(x)
 predictions = Dense(162, activation='softmax')(x)
 model_places = Model(inputs=base_model.input, outputs=predictions)
 model_places.load_weights('metadata/places_weights.hdf5')
+
+base_model = InceptionV3(weights=None, include_top=False)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(200, activation='softmax')(x)
+model_places = Model(inputs=base_model.input, outputs=predictions)
+model_places.load_weights('metadata/birds_weights.hdf5')
 
 del (base_model)
 del (x)
@@ -87,6 +104,47 @@ def image_pass_places(input_img):
         }
     return return_dict
 
+def image_pass_birds(input_img):
+    (latitude, longitude, date, time) = exif_extract_information(input_img)
+    img_target_size = (299,299)
+    img = prepare_image(input_img, img_target_size)
+    
+    with global_graph.as_default():
+        features = model_places.predict( img )
+    
+    cat_list = []
+    prob_list = []
+
+    for x in range( 0 , 200 ):
+        cat_list.append( birds_cats[x] )
+        prob_list.append( features[0 , x] )
+
+    sorted_list = sort_together( [prob_list , cat_list] , reverse=True )
+    prob_list = sorted_list[0]
+    cat_list = sorted_list[1]
+
+    top_5_cat = cat_list[0:5]
+
+    jsonString = []
+
+    for each_x in top_5_cat:
+        each_x = each_x.split('.')
+        x = re.sub( r'[^a-zA-Z ]+' , ' ' , each_x[1] )
+        x = x.title()
+        jsonString.append( x )
+
+
+    return_dict = \
+        {
+            "tags": jsonString,
+            "latitude": latitude,
+            "longitude": longitude,
+            "date": date,
+            "time": time,
+        }
+
+    return return_dict
+
 @app.route('/predict_places', methods=["POST"])
 def get_response_places():
     return_response = {"success": 0}
@@ -123,10 +181,45 @@ def get_response_places():
 
     return flask.jsonify(return_response)
 
+@app.route('/predict_birds', methods=["POST"])
+def get_response_birds():
+    return_response = {"success": 0}
+
+    if flask.request.method == "POST":
+        if flask.request.files.get("image_bytes"):
+            img = flask.request.files["image_bytes"].read()
+            img = Image.open(io.BytesIO(img))
+
+            info_dict = image_pass_birds(img)
+
+            return_response["tags"] = info_dict["tags"]
+            return_response["latitude"] = info_dict["latitude"]
+            return_response["longitude"] = info_dict["longitude"]
+            return_response["date"] = info_dict["date"]
+            return_response["time"] = info_dict["time"]
+
+            return_response["success"] = 1
+
+        if flask.request.files.get("image_url"):
+            img_url = flask.request.files["image_url"].read()
+            img_request = requests.get(img_url.decode('UTF-8'))
+            img = Image.open(io.BytesIO(img_request.content))
+
+            info_dict = image_pass_birds(img)
+
+            return_response["tags"] = info_dict["tags"]
+            return_response["latitude"] = info_dict["latitude"]
+            return_response["longitude"] = info_dict["longitude"]
+            return_response["date"] = info_dict["date"]
+            return_response["time"] = info_dict["time"]
+
+            return_response["success"] = 1
+
+    return flask.jsonify(return_response)
 
 @app.route('/')
 def print_main():
-    return '<h1>Welcome to TagPakistan ML API for Places2</h1>'
+    return '<h1>Welcome to TagPakistan ML API</h1>'
 
 
 if __name__ == '__main__':
